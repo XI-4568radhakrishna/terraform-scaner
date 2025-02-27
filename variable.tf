d49ef60912d84bc5e36309190bc8a2cd719b823b
XI-4568radhakrishna <Radhakrishna.Mopuru@xebia.com>
2025-02-27 17:20:30 +0530

    Update variable.tf
    variables updated

--- variable.tf
+++ variable.tf
@@ -124,23 +124,23 @@ variable "metadata_options" {
   }
 }
 
-/*variable "cpu_options" {
+variable "cpu_options" {
   description = "Defines CPU options to apply to the instance at launch time."
   type        = any
   default     = {}
-}*/
+}
 
-/*variable "cpu_core_count" {
+variable "cpu_core_count" {
   description = "Sets the number of CPU cores for an instance" # This option is only supported on creation of instance type that support CPU Options https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-optimize-cpu.html#cpu-options-supported-instances-values
   type        = number
   default     = null
-}*/
+}
 
-/*variable "cpu_threads_per_core" {
+variable "cpu_threads_per_core" {
   description = "Sets the number of CPU threads per core for an instance (has no effect unless cpu_core_count is also set)"
   type        = number
   default     = null
-}*/
+}
 
 ################################################################################
 # IAM Role / Instance Profile
